dcac676461893cf26d5446d4679c4f09d4f1e802
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2024-07-18 10:00:31 +0200

    include new app

---
+++ templates/quarkus-app/skeleton/src/main/java/org/gmagnotta/EchoResource.java
@@ -0,0 +1,63 @@
+package org.gmagnotta;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.logging.Logger;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+@Path("/api")
+public class EchoResource {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    SecurityIdentity securityIdentity;
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+
+        return "Hello world!";
+
+    }
+
+    @GET
+    @Path("/user")
+    @RolesAllowed("user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User user() {
+
+        return new User(securityIdentity);
+
+    }
+
+    @GET
+    @Path("/admin")
+    @RolesAllowed("admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String admin() {
+        return "granted";
+    }
+
+    public static class User {
+
+        private final String userName;
+
+        User(SecurityIdentity securityIdentity) {
+            this.userName = securityIdentity.getPrincipal().getName();
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+    }
+
+}
